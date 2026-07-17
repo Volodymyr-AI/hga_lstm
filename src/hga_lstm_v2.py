28a3a36d4bc50fc1160103dd5ddaf3d7978a8270
Volodymyr <volodymyr@getdentalray.com>
2026-07-17 12:27:34 +0300

    added comments

--- src/hga_lstm_v2.py
+++ src/hga_lstm_v2.py
@@ -49,7 +49,7 @@ HP_BOUNDS: list[tuple[float, float]] = [
     (16.0,  256.0), # hidden_size
     (1.0,   4.0), # num_layers
     (0.0,   0.5), # dropout
-    (1e-5,  1e-2), # learning_rate:  Adam optimizer
+    (1e-5,  1e-2), # learning_rate
     (8.0,   128.0), # batch_size
     (5.0,   50.0), # seq_len
 ]
